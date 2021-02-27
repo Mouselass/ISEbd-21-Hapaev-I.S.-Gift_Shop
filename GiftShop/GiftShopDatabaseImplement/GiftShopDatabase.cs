e12d5d4fa9b56a01dbd61a2a65b9816468ec5897
Mouselass <ivan.hapaev@yandex.ru>
2021-02-27 19:46:30 +0400

    lab3

---
+++ GiftShop/GiftShopDatabaseImplement/GiftShopDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GiftShopDatabaseImplement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiftShopDatabaseImplement
+{
+    public class GiftShopDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=WIN-7QPLO386PS9;Initial Catalog=GiftShopDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Component> Components { set; get; }
+        public virtual DbSet<Gift> Gifts { set; get; }
+        public virtual DbSet<GiftComponent> GiftComponents { set; get; }
+        public virtual DbSet<Order> Orders { set; get; }
+    }
+}
