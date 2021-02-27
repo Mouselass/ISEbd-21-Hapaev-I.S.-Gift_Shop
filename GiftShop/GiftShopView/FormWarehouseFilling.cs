a21f598a4dbf6e339abefa46ba6071169ef19474
Mouselass <ivan.hapaev@yandex.ru>
2021-02-27 20:09:31 +0400

    fix

--- GiftShop/GiftShopView/FormWarehouseFilling.cs
+++ GiftShop/GiftShopView/FormWarehouseFilling.cs
@@ -23,11 +23,18 @@ namespace GiftShopView
         public int WarehouseId { get { return Convert.ToInt32(comboBoxWarehouse.SelectedValue); } set { comboBoxWarehouse.SelectedValue = value; } }
         public int Count { get { return Convert.ToInt32(textBoxCount.Text); } set { textBoxCount.Text = value.ToString(); } }
 
+        public string ComponentName { get { return comboBoxComponent.Text; } }
+
+        WarehouseLogic Wlogic;
+
+        WarehouseBindingModel warehouseBindingModel = new WarehouseBindingModel();
+
         public FormWarehouseFilling(ComponentLogic logicC, WarehouseLogic logicW)
         {
             InitializeComponent();
             List<ComponentViewModel> listComponent = logicC.Read(null);
             List<WarehouseViewModel> listWarehouse = logicW.Read(null);
+            Wlogic = logicW;
             if (listComponent != null)
             {
                 comboBoxComponent.DisplayMember = "ComponentName";
@@ -61,6 +68,31 @@ namespace GiftShopView
                 MessageBox.Show("Выберите склад", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+
+            WarehouseViewModel view = Wlogic.Read(new WarehouseBindingModel
+            {
+                Id = WarehouseId
+            })?[0];
+
+            if (view != null)
+            {
+                warehouseBindingModel.WarehouseComponents = view.WarehouseComponents;
+                warehouseBindingModel.DateCreate = view.DateCreate;
+                warehouseBindingModel.Id = view.Id;
+                warehouseBindingModel.Responsible = view.Responsible;
+                warehouseBindingModel.WarehouseName = view.WarehouseName;
+            }
+
+            if (warehouseBindingModel.WarehouseComponents.ContainsKey(ComponentId))
+            {
+                int count = warehouseBindingModel.WarehouseComponents[ComponentId].Item2;
+                warehouseBindingModel.WarehouseComponents[ComponentId] = (ComponentName, count + Count);
+            }
+            else
+            {
+                warehouseBindingModel.WarehouseComponents.Add(ComponentId, (ComponentName, Count));
+            }
+            Wlogic.CreateOrUpdate(warehouseBindingModel);
 
             DialogResult = DialogResult.OK;
             Close();
